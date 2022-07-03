735268f56564c75510b4a60bfdae5248d44534d7
Gustavo Jezini <Gustavojezini@gmail.com>
2022-07-03 17:28:33 -0300

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from '../../node_modules/next/head'
+import Image from '../../node_modules/next/image'
 import styles from '../styles/Home.module.css'
+
+const bla = 'bla';
 
 export default function Home() {
   return (
