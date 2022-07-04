e0144070c771b23fd0a530b4c3baee116a90ac3c
Gustavo Jezini <Gustavojezini@gmail.com>
2022-07-04 09:09:56 -0300

    feat: adc Estilos e um teste para o teste

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,7 +2,12 @@ import * as S from './styles'
 
 const Main = () => (
   <S.Wrapper>
-    <h1>React Avançado</h1>
+    <S.Logo src='/img/icon-192.png' alt='Imagem de um átomo e "React Avançado" escrito ao lado.'/>
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      Typescript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Illustration src='/img/hero-illustration.svg' alt='Um dev de frente para tela com código' />
   </S.Wrapper>
 )
 
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -7,9 +7,15 @@ describe('<Main />', () => {
     const { container } = render(<Main />)
 
     expect(
-      screen.getByRole('heading', { name: /react avançadooo/i })
+      screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('Should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({'background-color': '#06092b'})
   })
 })
